c70c96911fca72bef38a5abc8c43dfd042624757
Azhar Islam <azhartislam@gmail.com>
2022-04-24 20:06:50 +0100

    Add bottom view

---
+++ Clubhouse/Views/HomeFeed/BottomHomeView.swift
@@ -0,0 +1,43 @@
+//
+//  BottomHomeView.swift
+//  Clubhouse
+//
+//  Created by Azhar Islam on 24/04/2022.
+//
+
+import SwiftUI
+
+struct BottomHomeView: View {
+    var body: some View {
+        
+        ZStack {
+            
+            Image
+                .grid
+                .font(Font.Nunito.bold(size: 24))
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .padding(.trailing, 15)
+            
+            Button(action: {}, label: {
+                Label("Start a room", systemImage: "plus")
+            })
+            .padding(.horizontal, 16)
+            .padding(.vertical, 9)
+            .font(Font.Nunito.bold(size: 18))
+            .foregroundColor(.white)
+            .background(Color.customGreen)
+            .cornerRadius(21, antialiased: true)
+            
+        }
+        .frame(maxWidth: .infinity,
+               maxHeight: 105,
+               alignment: .top)
+        .background(LinearGradient(gradient: Gradient(colors: [Color.background.opacity(0.1), Color.background]), startPoint: .top, endPoint: .bottom))
+    }
+}
+
+struct BottomHomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        BottomHomeView()
+    }
+}
